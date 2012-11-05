cbc1989d2027e99dc9fa1c80350663e2a936f130
Jimmy <jimmybanegas93@gmail.com>
2012-11-04 21:03:06 -0600

    Segundo

---
+++ src/cine/Sala.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cine;
+
+/**
+ *
+ * @author Auditor3
+ */
+public class Sala {
+    protected String tipo;
+    
+}
