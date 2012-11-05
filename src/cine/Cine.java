647eaee9af67ee83d1b481f8b22e3602adcf83b1
Jimmy <jimmybanegas93@gmail.com>
2012-11-04 20:43:28 -0600

    Primer

---
+++ src/cine/Cine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cine;
+
+/**
+ *
+ * @author Auditor3
+ */
+public class Cine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
